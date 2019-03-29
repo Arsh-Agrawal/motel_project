a41022844abf79c4e056025e75fc4f1fa3d4152e
Arsh-Agrawal <arsh.167.agrawal@gmail.com>
2019-03-29 14:16:07 +0530

    frame making

---
+++ src/motel/first.java
@@ -0,0 +1,46 @@
+package motel;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class first extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					first frame = new first();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public first() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblHelloBoi = new JLabel("hello boi");
+		lblHelloBoi.setBounds(27, 33, 69, 26);
+		contentPane.add(lblHelloBoi);
+	}
+}
