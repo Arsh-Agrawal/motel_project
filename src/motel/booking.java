0ae7c8d24f1808f86590d14d1fa0431a4297f4dc
Arsh-Agrawal <arsh.167.agrawal@gmail.com>
2019-04-05 13:46:27 +0530

    getting error in storing dates

--- src/motel/booking.java
+++ src/motel/booking.java
@@ -1,6 +1,8 @@
 package motel;
 
 import java.awt.BorderLayout;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -10,15 +12,21 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.awt.event.ActionEvent;
+import java.sql.Date;
 
 public class booking extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField chckin;
 	private JTextField chkout;
-	private JTextField uid;
+	private JTextField userid;
 
 	/**
 	 * Launch the application.
@@ -74,34 +82,127 @@ public class booking extends JFrame {
 		contentPane.add(chkout);
 		chkout.setColumns(10);
 		
-		JButton btnSubmit = new JButton("Submit");
-		btnSubmit.setBounds(105, 228, 117, 29);
-		contentPane.add(btnSubmit);
-		
 		JLabel return_msg = new JLabel("");
 		return_msg.setHorizontalAlignment(SwingConstants.CENTER);
 		return_msg.setBounds(273, 193, 160, 79);
 		contentPane.add(return_msg);
 		
+		JRadioButton rb1 = new JRadioButton("Single");
+		rb1.setBounds(105, 130, 141, 23);
+		contentPane.add(rb1);
+		
+		JRadioButton rb2 = new JRadioButton("Double");
+		rb2.setBounds(105, 165, 141, 23);
+		contentPane.add(rb2);
+		
+		JRadioButton rb3 = new JRadioButton("Deluxe");
+		rb3.setBounds(105, 200, 141, 23);
+		contentPane.add(rb3);
+		
+		ButtonGroup bgroup = new ButtonGroup();
+		bgroup.add(rb1);
+		bgroup.add(rb2);
+		bgroup.add(rb3);
+		
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String checkin = chckin.getText();
+				String checkout = chkout.getText();
+				String user_id = userid.getText();
+				
+				if(( userid != null || !user_id.isEmpty()) && (checkin != null || !checkin.isEmpty()) && (chkout != null || !checkout.isEmpty()))
+				{
+					int uid = Integer.parseInt(user_id); 
+					java.sql.Date chin = new java.sql.Date();
+					java.sql.Date chout = new java.sql.Date();
+					SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
+					try 
+					{
+						chin = format.parse(checkin);
+						chout = format.parse(checkout);
+					}
+					catch(Exception error)
+					{
+						String msg = "not a date fromat";
+						return_msg.setText(msg);
+					}
+					
+					int room_type;
+					
+					if(rb1.isSelected())
+					{
+						room_type = 1;
+					}
+					else if(rb2.isSelected())
+					{
+						room_type = 2;
+					}
+					else if(rb3.isSelected())
+					{
+						room_type = 3;
+					}
+					else
+					{
+						room_type = 0;
+						return_msg.setText("Please choose type of room");
+					}
+					
+					String qry = "Select count(*) as count from room where status = 0 and type_id = ? ";
+					PreparedStatement stmt = con.prepareStatement(qry);
+					stmt.setInt(1,room_type);
+					ResultSet rs = stmt.executeQuery();
+					
+					int count = rs.getInt("count");
+					
+					if(count == 0 )
+					{
+						return_msg.setText("Sorry, Particular room type is unavailable at the moment");						
+					}
+					else
+					{
+						qry = "Select room_no, floor from room where status = 0 and type_id = ?";
+						stmt = con.prepareStatement(qry);
+						stmt.setInt(1,room_type);
+						rs = stmt.executeQuery();
+						
+						//retrieving the first row(tuple)
+						
+						int room_no = rs.getInt("room_no");
+						int floor = rs.getInt("floor");
+						
+						qry = "insert into books values (?,?,?,?)";
+						stmt = con.prepareStatement(qry);
+						stmt.setInt(1,uid);
+						stmt.setInt(2, room_type);
+						stmt.setDate(3, chin);
+						
+						rs = stmt.executeQuery();
+//						String msg = "";
+						
+						
+					}
+					
+				}
+				
+				
+			}
+		});
+		btnSubmit.setBounds(105, 228, 117, 29);
+		contentPane.add(btnSubmit);		
+		
 		JLabel lblCid = new JLabel("User ID:");
 		lblCid.setBounds(255, 43, 68, 16);
 		contentPane.add(lblCid);
 		
-		uid = new JTextField();
-		uid.setBounds(321, 38, 80, 26);
-		contentPane.add(uid);
-		uid.setColumns(10);
+		userid = new JTextField();
+		userid.setBounds(321, 38, 80, 26);
+		contentPane.add(userid);
+		userid.setColumns(10);
 		
-		JRadioButton rdbtnSingle = new JRadioButton("Single");
-		rdbtnSingle.setBounds(105, 130, 141, 23);
-		contentPane.add(rdbtnSingle);
 		
-		JRadioButton rdbtnDouble = new JRadioButton("Double");
-		rdbtnDouble.setBounds(105, 165, 141, 23);
-		contentPane.add(rdbtnDouble);
 		
-		JRadioButton rdbtnDeluxe = new JRadioButton("Deluxe");
-		rdbtnDeluxe.setBounds(105, 200, 141, 23);
-		contentPane.add(rdbtnDeluxe);
+		
 	}
 }
